75fd647323d7488ff17acda56ce8e822d8c6fb4b
Lon P <lonpalmer@outlook.com>
2023-06-06 13:59:03 -0400

    prettier

--- src/main.mjs
+++ src/main.mjs
@@ -1,7 +1,6 @@
-import {log} from "./logger.mjs";
+import { log } from "./logger.mjs";
 import * as ver from "./generated/running_version.mjs";
 
 Hooks.on("init", () => {
   log(`Initializing Round Tracker version v${ver.runningVersion}`);
 });
-
