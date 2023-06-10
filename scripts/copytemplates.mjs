299315b11ce2e241fce29918ece99eaa37eee8e9
Lon P <lonpalmer@outlook.com>
2023-06-09 20:38:00 -0400

    added tailwind and learning templates

---
+++ scripts/copytemplates.mjs
@@ -0,0 +1,7 @@
+import { copy } from "fs-extra";
+
+// Build script that copies the templates to the dist folder.
+
+export async function copyTemplates() {
+  await copy("src/templates", "dist/templates");
+}
