b1974ba9cba9c8d671698ec0b527a30027f12649
Lon P <lonpalmer@outlook.com>
2023-07-14 21:43:13 -0500

    Minor updates to the hud

--- src/hud.mjs
+++ src/hud.mjs
@@ -1,3 +1,12 @@
+
+/**
+ * Make the button for the HUD.  This button is displayed on the token HUD
+ * when it's right-clicked.
+ * @param {Object} hudDoc Foundry document for the HUD
+ * @param {JQuery} html JQuery object for the HUD
+ * @param {Object} token Foundry Token object
+ * @returns {JQuery} JQuery object for the button
+ */
 export async function makeHudButton(hudDoc, html, token) {
   const buttonTemplate = await renderTemplate(
     "modules/round-tracker/templates/hud_button.html",
